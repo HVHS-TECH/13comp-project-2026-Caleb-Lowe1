354e5c4b943d667916f39d85d9d3ac58105865a7
Caleb-Lowe1 <22026cl@hvhs.school.nz>
2026-06-25 12:53:53 +1200

    Beta V0.2.8

--- fb_io.mjs
+++ fb_io.mjs
@@ -939,13 +939,7 @@ location.href = "./GTNlobby.html"
 
 })
 
-onValue (guestwinner, (snapshot) => {
-const guestwin = snapshot.val();
-if (guestwin != null && guestwin.iswinner == true){
-alert("You lost, your opponent guessed the correct number. The correct number was " + guessNumber)
-location.href = "./GTNlobby.html"
-}
-})
+
 
 
 }
